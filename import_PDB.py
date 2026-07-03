49b5e34053dd016bac10f009df29fc771835b64a
alicejegou <127987067+alicejegou@users.noreply.github.com>
2026-07-03 15:23:54 +0200

    Update PDB file path and improve atom creation logic

--- import_PDB.py
+++ import_PDB.py
@@ -2,13 +2,26 @@ import bpy
 import random
 
 # Path to your .PDB file
-pdb_file="C:/Users/AJ269416.INTRA/Downloads/fBOX.pdb"
+pdb_file="C:/Users/AJ269416.INTRA/Downloads/bZIP3.pdb"
 
-# Initiate object collections to store atoms
+# Init collections
+collection = bpy.data.collections.new("Proteine")
+bpy.context.scene.collection.children.link(collection)
+
+# Creating spheres meshes for atoms 
+bpy.ops.mesh.primitive_uv_sphere_add(radius=1.0)  # Change as you want atoms base radius
+sphere_template = bpy.context.active_object
+sphere_template.name = "Atom_Template"
+sphere_template.hide_viewport = True
+sphere_template.hide_render = True
+
+atom_mesh = sphere_template.data
+
+# Init collections
 collection = bpy.data.collections.new("Atoms")
 bpy.context.scene.collection.children.link(collection)
 
-# Read .PDB and extract atoms spatial data
+# Parse .PDB file to extract atoms disposition 
 with open(pdb_file, "r") as f:
     lines = f.readlines()
 
@@ -18,7 +31,7 @@ for line in lines:
         x = float(line[30:38].strip())
         y = float(line[38:46].strip())
         z = float(line[46:54].strip())
-        scale = random.uniform(1.2, 1.5) # Ypiu can change here the random size of the atoms to get a globular effect
+        scale = random.uniform(1.2, 1.5) # You can canpley with individual atoms scales to create a globular effect
         new_sphere = bpy.data.objects.new(f"Atom_{atom_name}", atom_mesh)
         new_sphere.location = (x, y, z)
         new_sphere.scale = (scale, scale, scale)
